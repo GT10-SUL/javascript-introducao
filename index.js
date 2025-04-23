9cf1611cf5367e079ee37af03a1988ba2055f84a
gleidsonteixeira <gleidsondeveloper@gmail.com>
2025-04-23 16:50:56 -0300

    updates

---
+++ index.js
@@ -0,0 +1,52 @@
+// let nomeSobrenome = "Gleidson Rocha";
+// let idade = 34;
+// console.log(idade);
+
+// // idade = idade + 1;
+// idade += 1;
+// console.log(idade);
+
+// let numero = 2;
+// // numero = numero * 2;
+// numero *= 2;
+// console.log(numero);
+
+// console.log("2 == 2: ", 2 == 2); 
+// console.log("false == true: ", false == true); 
+// console.log("Gleidson == gleidson: ", "Gleidson" == "gleidson"); 
+// console.log("2 == 3: ", 2 == 3); 
+// console.log("2 != 2: ", 2 != 2); 
+// console.log("2 != 3: ", 2 != 3); 
+
+// let idadeAlistamento = 18;
+// let minhaIdade = 19;
+// console.log(minhaIdade >= idadeAlistamento);
+// console.log(idadeAlistamento <= minhaIdade);
+
+// console.log("2 == 2: ", 2 == 2); 
+// console.log("2 == 2: ", 2 == "2"); 
+// console.log("2 === 2: ", 2 === 2); 
+// console.log("2 === 2: ", 2 === "2"); 
+// console.log("2 != 2: ", 2 != 2); 
+// console.log("2 != 2: ", 2 != "2"); 
+// console.log("2 !== 2: ", 2 !== 2); 
+// console.log("2 !== 2: ", 2 !== "2"); 
+
+// let numero = 4;
+
+// console.log("2 == 2: ", numero == 2 && numero % 2 == 0); 
+// console.log("2 == 2: ", numero == 2 || numero % 2 == 0); 
+
+// let verdadeiro = true;
+// console.log(verdadeiro);
+// console.log(!verdadeiro);
+// verdadeiro = !verdadeiro;
+// console.log(!verdadeiro);
+
+
+
+
+
+
+
+
